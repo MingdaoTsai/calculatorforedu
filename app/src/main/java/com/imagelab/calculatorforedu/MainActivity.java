74ee67e400e9ebfe72f5fc23f4c9202328b396bb
Jacob Tsai <jacob.tsai@hotmail.com>
2019-01-06 20:48:38 +0800

    test pc first commit

--- app/src/main/java/com/imagelab/calculatorforedu/MainActivity.java
+++ app/src/main/java/com/imagelab/calculatorforedu/MainActivity.java
@@ -14,6 +14,7 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
+    //test pc sync
     boolean initstat= true;
     Button button_c;
     Button button0;
